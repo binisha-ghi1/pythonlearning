f062b4bf4f9056b7ca5008b990b0889349632630
binisha-ghi1 <binishaghimire126@gmail.com>
2025-07-10 12:41:15 +0545

    code done

---
+++ day1.py
@@ -0,0 +1,15 @@
+#modules
+#2. build in modules
+#1. external modules
+#comments
+#multiline comments
+#single line comments
+# print("Hello World")
+
+#write a program to print twinkle twinkle little star poem in python.
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
